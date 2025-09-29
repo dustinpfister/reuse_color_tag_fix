c3ad2e69d27c678c54492abe4f103a1854baa8e2
dustinpfister <dustin.pfister@gmail.com>
2025-09-29 14:31:30 -0400

    display_full_menu helper

--- dist/chrome_extension/popup.js
+++ dist/chrome_extension/popup.js
@@ -1,26 +1,43 @@
+const el_enabled = document.querySelector('#checkbox_enabled');
+const el_full_menu = document.querySelector('#full_menu');
+const el_mode = document.querySelector('#select_mode');
+/********* **********
+HELPER FUNCTIONS
+********** *********/
+const reload_at_data1 = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const tab = tabs[0];
+        const parts = tab.url.replace('https://', '').split('/');
+        if(parts.length >= 3 && parts[0] === 'data1.ithacareuse.org' && parts[1] === 'pricing' && parts[2] === 'portal'){
+            chrome.tabs.reload();
+        }
+    });
+};
+const display_full_menu = (bool=true) => {
+    el_full_menu.style.visibility = 'visible';
+    if(!bool){
+        el_full_menu.style.visibility = 'hidden';
+    }
+};
 /********* **********
 SETUP checkbox enabled
 ********** *********/
-const el_enabled = document.querySelector('#checkbox_enabled');
 chrome.storage.local.get('enabled')
 .then((result) => {
    const bool = result['enabled'];
    el_enabled.checked = bool;
+   display_full_menu(bool);
 });
 el_enabled.addEventListener('change', (e) => {
     chrome.storage.local.set({ enabled : e.target.checked })
     .then(()=>{
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            const tab = tabs[0];
-            const parts = tab.url.replace('https://', '').split('/');
-            if(parts.length >= 3 && parts[0] === 'data1.ithacareuse.org' && parts[1] === 'pricing' && parts[2] === 'portal'){
-                chrome.tabs.reload();
-            }
-        });
-    })
+        reload_at_data1();
+        display_full_menu(e.target.checked);
+    });
 });
-
-const el_mode = document.querySelector('#select_mode');
+/********* **********
+SETUP select mode
+********** *********/
 chrome.storage.local.get('mode')
 .then((result) => {
     el_mode.value = result['mode'];
@@ -29,5 +46,6 @@ el_mode.addEventListener('change', (e) => {
     chrome.storage.local.set({ mode : e.target.value })
     .then(() => {
         console.log( 'mode set to :' + e.target.value );
+        reload_at_data1();
     });
 });
